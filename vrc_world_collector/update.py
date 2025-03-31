1d63b81661aa05a7428bc1a22c6bd84e07cdb5bf
prprpurin <waket26@gmail.com>
2025-03-31 22:51:26 +0900

    VRCから自動でワールドの更新をするように

--- vrc_world_collector/update.py
+++ vrc_world_collector/update.py
@@ -29,29 +29,40 @@ def parse_world_id(url: str):
 
 
 def main():
-    load_dotenv()
-    notion_manager = get_notion_manager()
+    # TODO: 専用のコマンドを作成して、環境変数を読み込むようにする
+    # load_dotenv()
 
-    print('Step1. 登録済みワールド一覧のIDを取得')
-    pages = notion_manager.get_raw_values()
+    try:
+        print('Step1. 登録済みワールド一覧のIDを取得')
+        notion_manager = get_notion_manager()
+        pages = notion_manager.get_raw_values()
+    except Exception as e:
+        # 中断しないようにする
+        print(f"Notion API からページを取得できませんでした: {e}")
+        return
 
-    with vrchatapi.ApiClient() as api_client:
-        for page in pages:
-            properties = page.get('properties', {})
-            page_id = page.get('id')
-            world_id = properties.get('ID', {}).get('rich_text', [])[0]['plain_text']
-            world_api = get_world_api(api_client)
-            world_info = vrchat.get_world_info(world_api, world_id)
+    try:
+        with vrchatapi.ApiClient() as api_client:
+            print('Step2. VRChat API から Notion に登録済みワールドの情報を取得')
+            for page in pages:
+                properties = page.get('properties', {})
+                page_id = page.get('id')
+                world_id = properties.get('ID', {}).get('rich_text', [])[0]['plain_text']
+                world_api = get_world_api(api_client)
+                world_info = vrchat.get_world_info(world_api, world_id)
 
-            update_properties = {
-                'Description': NotionPropertyBuilder.rich_text(world_info['description']),
-                'Author': NotionPropertyBuilder.rich_text(world_info['author']),
-                'PublicationDate': NotionPropertyBuilder.date(world_info['publicationDate']),
-            }
-            updated_page = notion_manager.update_page_properties(page_id, update_properties, world_info['name'])
-            if updated_page:
-                print(f"ページID: {updated_page['id']} を更新しました")
-
+                update_properties = {
+                    'Description': NotionPropertyBuilder.rich_text(world_info['description']),
+                    'Author': NotionPropertyBuilder.rich_text(world_info['author']),
+                    'PublicationDate': NotionPropertyBuilder.date(world_info['publicationDate']),
+                }
+                updated_page = notion_manager.update_page_properties(page_id, update_properties, world_info['name'])
+                if updated_page:
+                    print(f"ページID: {updated_page['id']} を更新しました")
+    except Exception as e:
+        # 中断しないようにする
+        print(f"エラーが発生しました: {e}")
+        return
 
 
 if __name__ == '__main__':
